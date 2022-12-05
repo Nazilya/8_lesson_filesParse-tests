80e7e1360283015b338ddfcc631d893b083b55bf
Nazilya Mullagildina <mullagildina.n@yandex.ru>
2022-12-05 13:05:30 +0500

    lesson8 - files

---
+++ src/test/java/guru/qa/lesson8/model/Glossary.java
@@ -0,0 +1,14 @@
+package guru.qa.lesson8.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Glossary {
+    public String title;
+    @SerializedName("gloss_div")
+    public GlossDiv glossDiv;
+
+    public static class GlossDiv {
+        public String title;
+        public Boolean flag;
+    }
+}
